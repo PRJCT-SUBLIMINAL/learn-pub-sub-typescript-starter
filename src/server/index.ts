cb960153d228f588b6fdb5c5948787030dd8e3ff
Tyler <tyler110597@gmail.com>
2026-06-13 23:52:49 +0100

    init

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,19 @@
+import amqp from "amqplib";
+
 async function main() {
-  console.log("Starting Peril server...");
+  const CONNECTION_STRING: string = "amqp://guest:guest@localhost:5672/";
+  const connection = await amqp.connect(CONNECTION_STRING);
+  if (!connection) {
+    throw new Error("Cannot establish connection");
+  } else {
+    console.log("Connection successful!");
+  }
+
+  process.on("SIGINT", async ()=>{
+    console.log("Shutting down.");
+    await connection.close();
+    process.exit(0);
+  })
 }
 
 main().catch((err) => {
